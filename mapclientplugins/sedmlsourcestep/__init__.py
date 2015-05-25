74f3bfdeb50c178a3c444a2f0860c78b91c11f09
Andre <nickerso@users.sf.net>
2015-05-25 22:12:55 +1200

    adding initial plugin created by Hugh for defining a SED-ML file source

---
+++ mapclientplugins/sedmlsourcestep/__init__.py
@@ -0,0 +1,13 @@
+
+'''
+MAP Client Plugin
+'''
+
+__version__ = '0.1.0'
+__author__ = 'Andre'
+__stepname__ = 'SED-ML Source'
+__location__ = ''
+
+# import class that derives itself from the step mountpoint.
+from mapclientplugins.sedmlsourcestep import step
+
